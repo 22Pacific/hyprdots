b8e63be90181363f10333df4e71725a4c8c122dd
22Pacific <pk22.kumarprashant@gmail.com>
2024-10-04 01:03:02 +0530

    Remove nvidia dkms driver installation

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -8,11 +8,11 @@ cat << "EOF"
 
 -------------------------------------------------
         .
-       / \         _       _  _      ___  ___ 
+       / \         _       _  _      ___  ___
       /^  \      _| |_    | || |_  _|   \| __|
-     /  _  \    |_   _|   | __ | || | |) | _| 
+     /  _  \    |_   _|   | __ | || | |) | _|
     /  | | ~\     |_|     |_||_|\_, |___/|___|
-   /.-'   '-.\                  |__/          
+   /.-'   '-.\                  |__/
 
 -------------------------------------------------
 
@@ -96,18 +96,6 @@ EOF
     if [ -f "${cust_pkg}" ] && [ ! -z "${cust_pkg}" ]; then
         cat "${cust_pkg}" >> "${scrDir}/install_pkg.lst"
     fi
-
-    #--------------------------------#
-    # add nvidia drivers to the list #
-    #--------------------------------#
-    if nvidia_detect; then
-        cat /usr/lib/modules/*/pkgbase | while read krnl; do
-            echo "${krnl}-headers" >> "${scrDir}/install_pkg.lst"
-        done
-        nvidia_detect --drivers >> "${scrDir}/install_pkg.lst"
-    fi
-
-    nvidia_detect --verbose
 
     #----------------#
     # get user prefs #
